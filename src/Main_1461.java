aaa61836f5598bdc05972ee6e8c0f61c82c3b56c
cdlicn <619356147@qq.com>
2026-02-23 20:33:57 +0800

    0223

---
+++ src/Main_1461.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main_1461 {
+    public boolean hasAllCodes(String s, int k) {
+        Set<String> set = new HashSet<>();
+        for (int i = k; i <= s.length(); i++) {
+            set.add(s.substring(i - k, i));
+        }
+        return set.size() == (1 << k);
+    }
+}
