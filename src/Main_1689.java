54adc73d8c6c0dfafb164c6c9d23fba050fc7cbf
cdlicn <92195884+cdlicn@users.noreply.github.com>
2026-03-01 21:01:22 +0800

    0301

---
+++ src/Main_1689.java
@@ -0,0 +1,10 @@
+public class Main_1689 {
+    public int minPartitions(String n) {
+        for (int i = 9; i >= 0; i--) {
+            if (n.contains(i + "")) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
