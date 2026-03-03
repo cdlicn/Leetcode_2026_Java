c6744e0947753a5da2bc8e066910c7fbe6ea9603
cdlicn <92195884+cdlicn@users.noreply.github.com>
2026-03-03 10:09:54 +0800

    0303

---
+++ src/Main_1545.java
@@ -0,0 +1,15 @@
+public class Main_1545 {
+    public char findKthBit(int n, int k) {
+        if (n == 1) {
+            return '0';
+        }
+        if (k == 1 << (n - 1)) {
+            return '1';
+        }
+        if (k < 1 << (n - 1)) {
+            return findKthBit(n - 1, k);
+        }
+        char res = findKthBit(n - 1, (1 << n) - k);
+        return (char) (res ^ 1);
+    }
+}
