2b3e9600ae61e09c080d7140bd1f56baafbdd83d
cdlicn <619356147@qq.com>
2026-05-27 23:54:21 +0800

    0527

--- src/Main_3120.java
+++ src/Main_3120.java
@@ -8,12 +8,29 @@ import java.util.Set;
  * @description
  */
 
+// TODO 3121. 统计特殊字母的数量 II
 public class Main_3120 {
     public int numberOfSpecialChars(String word) {
-        int[] mask = new int[2];
+        int ans = 0;
+        int[] state = new int[27];
         for (char c : word.toCharArray()) {
-            mask[c >> 5 & 1] |= 1 << (c & 31);
+            int x = c & 31; // 转成数字 1~26
+            if ((c & 32) > 0) { // 小写字母
+                if (state[x] == 0) {
+                    state[x] = 1;
+                } else if (state[x] == 2) { // 大写的后面不能有小写
+                    state[x] = -1;
+                    ans--;
+                }
+            } else { // 大写字母
+                if (state[x] == 0) { // 还没遇到小写，就先遇到大写了
+                    state[x] = -1;
+                } else if (state[x] == 1) {
+                    state[x] = 2;
+                    ans++;
+                }
+            }
         }
-        return Integer.bitCount(mask[0] & mask[1]);
+        return ans;
     }
 }
